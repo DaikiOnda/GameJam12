442d49e36225faa06290feb18ed7de5cf2dbd53b
DaikiOnda <daiki281@outlook.jp>
2022-12-28 19:19:07 +0900

    3年ですべてブラック企業になるように

--- Assets/Daiki/Source/Next.cs
+++ Assets/Daiki/Source/Next.cs
@@ -7,6 +7,8 @@ public class Next : MonoBehaviour
     public GameObject  benefit;
     public GameObject black;
     public GameObject searcher;
+    private bool flag=false;
+    public int year=0;
     ProductBlackco produckblackco;
     Searchreply searchreply;
     //BlackC blackC;
@@ -16,13 +18,25 @@ public class Next : MonoBehaviour
     }
     void Update()
     {
-        if((GManager.instance.time)%12==0){
+        if(((GManager.instance.time)%12==0)&&flag){
             produckblackco.Duringproduct=31;
+            flag=false;
+            if((GManager.instance.time)%24==0){
+                year++;
+                if(year==3){
+                    produckblackco.Days=400;
+                }
+            }
         }
+        else if((GManager.instance.time)%12!=0) flag=true;
     }
     //ボタンを押すと次のターンに
     public void GoNextTurn()
     {
+        searchreply.Searchbool=false;
+        searchreply.panel.SetActive(false);
+        //searchreply.reaction.SetActive(false);
+        searchreply.Waittime=false;
         GManager.instance.turn=(GManager.instance.turn+1)%4;
         benefit.SetActive(false);
         if(GManager.instance.turn==2){
@@ -38,10 +52,7 @@ public class Next : MonoBehaviour
                 }
                 else    GManager.instance.turn=(GManager.instance.turn+1)%4;
             }
-            searchreply.panel.SetActive(false);
-            searchreply.reaction.SetActive(false);
-            searchreply.Waittime=false;
-            searchreply.Searchbool=false;
+            //searchreply.Searchbool=false;
         }
 
     }
