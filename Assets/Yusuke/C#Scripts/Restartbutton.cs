5b32fe7c0c801778d7904d03f56243c1d231fdc2
weisswein <archeologistspeshil@gmail.com>
2022-12-29 18:13:51 +0900

    ResultとStart画面の音を入れました

--- Assets/Yusuke/C#Scripts/Restartbutton.cs
+++ Assets/Yusuke/C#Scripts/Restartbutton.cs
@@ -5,10 +5,18 @@ using UnityEngine.SceneManagement;
 public class Restartbutton : MonoBehaviour
 {
     // Start is called before the first frame update
-  
+   private float step_time=0.0f;
+   bool timer =false;
+   
+   void Update(){
+   if(timer) step_time += Time.deltaTime;
+    //3秒後にメソッドを実行する
+   if(step_time >= 2.0f)
+        SceneManager.LoadScene("StartScene");
+   }
     public void Onclick(){
         Debug.Log("ボタンが押されました！");
-        SceneManager.LoadScene("StartScene");
+         timer=true;
     }
    
 }
